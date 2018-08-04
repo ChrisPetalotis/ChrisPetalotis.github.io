c35b4edce6c0d57c4baddeac58ea972defa99596
Christos Petalotis <pechriss@gmail.com>
2018-08-04 13:13:44 +0300

    added transition for the Go to top arrow

--- javascript.js
+++ javascript.js
@@ -4,33 +4,53 @@ $('a[href*="#"]')
   .not('[href="#"]')
   .not('[href="#0"]')
   .click(function(event) {
-    // On-page links - Links that navigate to the same page and do not lead to other page
+	// On-page links - Links that navigate to the same page and do not lead to other page
     if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      &&
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
+    	location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      	&&
+      	location.hostname == this.hostname
+    	) {
+      		// Figure out element to scroll to
+      		var target = $(this.hash);
+
+	      	target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	      	// Does a scroll target exist?
+	      	if (target.length) {
+	        	// Only prevent default if animation is actually gonna happen
+	        	event.preventDefault();
+	        	$('html, body').animate({
+	          	scrollTop: target.offset().top
+	        	}, 1000, function() {
+	          	// Callback after animation
+	          	// Must change focus!
+	          	var $target = $(target);
+	          	$target.focus();
+	          	if ($target.is(":focus")) { // Checking if the target was focused
+	            	return false;
+	          	} else {
+	            	$target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+	            	$target.focus(); // Set focus again
+	          	};
+	        	});
+	    	}
+   		}
   });
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	event.preventDefault();
+	$('html, body').animate({
+  		scrollTop: 0
+	}, 1000);
+}
