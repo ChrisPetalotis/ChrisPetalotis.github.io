1045fdcdf89303b488cab6eb6f0ca25e225e0e89
Christos Petalotis <17175412+ChrisPetalotis@users.noreply.github.com>
2018-10-11 15:01:59 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,47 +1,44 @@
 $().ready( () => {
-document.querySelector('#main').classList.add('visible');
+	document.querySelector('#main').classList.add('visible');
 
-// When clicking on a navbar link it goes to the corresponding part of the website
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-	// On-page links - Links that navigate to the same page and do not lead to other page
-    if (
-    	location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      	&&
-      	location.hostname == this.hostname
-    	) {
-      		// Figure out element to scroll to
-      		var target = $(this.hash);
+	// When clicking on a navbar link it goes to the corresponding part of the website
+	// Select all links with hashes
+	$('a[href*="#"]')
+	  // Remove links that don't actually link to anything
+	  .not('[href="#"]')
+	  .not('[href="#0"]')
+	  .click(function(event) {
+		// On-page links - Links that navigate to the same page and do not lead to other page
+	    if (
+		location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+		&&
+		location.hostname == this.hostname
+		) {
+			// Figure out element to scroll to
+			var target = $(this.hash);
 
-	      	target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-	      	// Does a scroll target exist?
-	      	if (target.length) {
-	        	// Only prevent default if animation is actually gonna happen
-	        	event.preventDefault();
-	        	$('html, body').animate({
-	          	scrollTop: target.offset().top
-	        	}, 1000, function() {
-	          	// Callback after animation
-	          	// Must change focus!
-	          	var $target = $(target);
-	          	$target.focus();
-	          	if ($target.is(":focus")) { // Checking if the target was focused
-	            	return false;
-	          	} else {
-	            	$target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-	            	$target.focus(); // Set focus again
-	          	};
-	        	});
-	    	}
-   		}
-  });
-
-	// When the user scrolls down 450px from the top of the document, show the button
-	window.addEventListener('scroll', scrollFunction);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			// Does a scroll target exist?
+			if (target.length) {
+				// Only prevent default if animation is actually gonna happen
+				event.preventDefault();
+				$('html, body').animate({
+				scrollTop: target.offset().top
+				}, 1000, function() {
+				// Callback after animation
+				// Must change focus!
+				var $target = $(target);
+				$target.focus();
+				if ($target.is(":focus")) { // Checking if the target was focused
+				return false;
+				} else {
+				$target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+				$target.focus(); // Set focus again
+				};
+				});
+			}
+			}
+	  });
 
 	function scrollFunction() {
 	    if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450)
@@ -49,18 +46,19 @@ $('a[href*="#"]')
 	    else 
 	        document.getElementById("arrow-to-top").classList.remove('active');
 	}
-
-
-});
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-	event.preventDefault();
-	$('html, body').animate({
-  		scrollTop: 0
-	}, 900);
-}
-
-// make the arrow fade out as we go to the next page 
-$(window).scroll(function() {
-    $(".arrowDown").css("opacity", 1 - $(window).scrollTop() / 250);
+	
+	// When the user scrolls down 450px from the top of the document, show the button
+	window.addEventListener('scroll', scrollFunction);
+	
+	// When the user clicks on the button, scroll to the top of the document
+	function topFunction() {
+		event.preventDefault();
+		$('html, body').animate({
+  			scrollTop: 0
+		}, 900);
+	}
+	// make the arrow fade out as we go to the next page 
+	$(window).scroll(function() {
+    	$(".arrowDown").css("opacity", 1 - $(window).scrollTop() / 250);
+});	
 });
