886700a862a02bbcea1e7826616f4cb2af7ed20d
potatohead <17175412+ChrisPetalotis@users.noreply.github.com>
2020-12-13 03:19:53 +0200

    Rebuilt most of the HTML using Bootstrap, Changed from CSS to SCSS for styles

--- javascript.js
+++ javascript.js
@@ -2,14 +2,16 @@ $().ready( () => {
 	// When the user scrolls down 450px from the top of the document, show the button
 	window.addEventListener('scroll', scrollFunction);
 
-	function scrollFunction() {
-	    if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450)
-	        document.getElementById("arrow-to-top").classList.add('active');
-	    else 
-	        document.getElementById("arrow-to-top").classList.remove('active');
-	}
+	
 });
 
+
+function scrollFunction() {
+	if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450)
+		document.getElementById("arrow-to-top").classList.add('active');
+	else 
+		document.getElementById("arrow-to-top").classList.remove('active');
+}
 // When the user clicks on the arrow button, scroll to the top of the document
 function topFunction() {
 	event.preventDefault();
